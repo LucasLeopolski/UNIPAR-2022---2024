12280d4a06e8f6bd3ba5f79657b0e62698a6f560
LucasLeopolski <126730157+LucasLeopolski@users.noreply.github.com>
2024-06-24 21:51:51 -0300

    Add files via upload

---
+++ 2024/EXERCICIOS/Conexao BD/conexaoBancoDados/src/main/java/com/mycompany/conexaobancodados/ConexaoBancoDados.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.conexaobancodados;
+
+/**
+ *
+ * @author Usuário
+ */
+public class ConexaoBancoDados {
+
+    public static void main(String[] args) {
+        new LoginView().setVisible(true);
+    }
+}
