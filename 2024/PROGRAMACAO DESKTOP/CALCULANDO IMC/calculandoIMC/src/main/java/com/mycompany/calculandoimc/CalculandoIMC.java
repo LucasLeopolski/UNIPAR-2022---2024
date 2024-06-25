1e690bdbcf1f7a6c3928940f3f29edc54dcbeee6
LucasLeopolski <126730157+LucasLeopolski@users.noreply.github.com>
2024-06-24 22:06:36 -0300

    Add files via upload

---
+++ 2024/PROGRAMACAO DESKTOP/CALCULANDO IMC/calculandoIMC/src/main/java/com/mycompany/calculandoimc/CalculandoIMC.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculandoimc;
+
+/**
+ *
+ * @author Usuário
+ */
+public class CalculandoIMC {
+
+    public static void main(String[] args) {
+        new FrameIMC().setVisible(true);
+    }
+}
