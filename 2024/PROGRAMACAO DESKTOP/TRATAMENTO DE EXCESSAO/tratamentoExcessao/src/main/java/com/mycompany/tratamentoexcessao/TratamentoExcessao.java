3fc455ab29e1f24812ba6227b9fff9d1e0cdaaf9
LucasLeopolski <126730157+LucasLeopolski@users.noreply.github.com>
2024-06-24 22:11:25 -0300

    Add files via upload

---
+++ 2024/PROGRAMACAO DESKTOP/TRATAMENTO DE EXCESSAO/tratamentoExcessao/src/main/java/com/mycompany/tratamentoexcessao/TratamentoExcessao.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tratamentoexcessao;
+
+/**
+ *
+ * @author Usuário
+ */
+public class TratamentoExcessao {
+
+    public static void main(String[] args) {
+        new FrameExcessao().setVisible(true);
+    }
+}
